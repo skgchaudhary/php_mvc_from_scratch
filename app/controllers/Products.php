bbe8def92fde7fc4212c2deac57f8322273794e1
skgchaudhary <skgchaudhary@gmail.com>
2025-03-03 15:04:07 +0530

    Add files via upload

---
+++ app/controllers/Products.php
@@ -0,0 +1,6 @@
+<?php
+    class Products extends Controller {
+        public function index() {
+            $this->view('product');
+        }
+    }
---
+++ app/controllers/_404.php
@@ -0,0 +1,6 @@
+<?php
+    class _404 extends Controller {
+        public function index() {
+            echo "This page does not exists.";
+        }
+    }
